fa6929763b703f6a4d3a9a0e9644039d04e61f79
daniel442li <daniel442li@gmail.com>
2022-02-28 21:23:05 -0600

    home page done

--- KC Potter/ContentView.swift
+++ KC Potter/ContentView.swift
@@ -17,19 +17,49 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
 
             Text("K.C. Potter Center at Vanderbilt")
-                .font(.title3)
+                .font(.title2)
+                .fontWeight(.bold)
                 .padding()
-                .frame(maxWidth: 300, alignment: .center)
-
+                .frame(maxWidth: 400, alignment: .center)
+            
+            Image("flag")
+                        .resizable()
+                        .frame(minWidth: 0, maxWidth: 380, maxHeight: 250)
+            
             Button(action: {
                 print("Delete tapped!")
             }) {
-                Text("Delete")
+                Text("Get Connected")
                     .fontWeight(.semibold)
                     .font(.title)
                     .foregroundColor(Color.black)
-                    .frame(minWidth: 0, maxWidth: 80)
+                    .frame(minWidth: 0, maxWidth: 380, maxHeight: 101)
                 .background(Color.red)
+                .cornerRadius(40)
+            }
+            .padding(.top, 50.0)
+            
+            Button(action: {
+                print("Delete tapped!")
+            }) {
+                Text("Get Involved")
+                    .fontWeight(.semibold)
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                    .frame(minWidth: 0, maxWidth: 380, maxHeight: 100)
+                .background(Color.orange)
+                .cornerRadius(40)
+            }
+            
+            Button(action: {
+                print("Delete tapped!")
+            }) {
+                Text("Find a Gender Neutral Bathroom")
+                    .fontWeight(.semibold)
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                    .frame(minWidth: 0, maxWidth: 380, maxHeight: 100)
+                .background(Color.yellow)
                 .cornerRadius(40)
             }
             
@@ -39,8 +69,22 @@ struct ContentView: View {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
