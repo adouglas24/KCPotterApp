ec997800f6e1ce9f0e9510790403b646a95a3337
zliang <zliang@petsmart.com>
2022-03-18 15:36:20 -0500

    finished questions page

--- KC Potter/questionsPage.swift
+++ KC Potter/questionsPage.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct questionsPage : View {
+
+    @State var value = ""
+    var affiliation = "I am a ..."
+    var affiliationList = ["Alumni", "Faculty", "Staff", "Student", "Sommunity"]
+    var identity = "I identify as ..."
+    var identityList = ["Lesbian/Gay", "Bi/Pan/Fluid", "Trans*", "Ally", "Intersex", "Ace/Aro"]
+    var type = "I am looking for ..."
+    var typeList = ["Athletics", "Counseling", "Educational", "Medical/Health", "Religious/Spiritual"]
+    
     @Binding var questions: Bool
     var body: some View {
         VStack (){
@@ -23,6 +32,75 @@ struct questionsPage : View {
                 .cornerRadius(40)
             }
         
+        }
+        
+        Menu {
+            ForEach(affiliationList, id: \.self){ client in
+                Button(client) {
+                    self.value = client
+                }
+            }
+        } label: {
+            VStack(spacing: 5){
+                HStack{
+                    Text(value.isEmpty ? affiliation : value)
+                        .foregroundColor(value.isEmpty ? .gray : .black)
+                    Spacer()
+                    Image(systemName: "chevron.down")
+                        .foregroundColor(Color.orange)
+                        .font(Font.system(size: 20, weight: .bold))
+                }
+                .padding(.horizontal)
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(height: 2)
+            }
+        }
+        
+        Menu {
+            ForEach(identityList, id: \.self){ client in
+                Button(client) {
+                    self.value = client
+                }
+            }
+        } label: {
+            VStack(spacing: 5){
+                HStack{
+                    Text(value.isEmpty ? identity : value)
+                        .foregroundColor(value.isEmpty ? .gray : .black)
+                    Spacer()
+                    Image(systemName: "chevron.down")
+                        .foregroundColor(Color.orange)
+                        .font(Font.system(size: 20, weight: .bold))
+                }
+                .padding(.horizontal)
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(height: 2)
+            }
+        }
+        
+        Menu {
+            ForEach(typeList, id: \.self){ client in
+                Button(client) {
+                    self.value = client
+                }
+            }
+        } label: {
+            VStack(spacing: 5){
+                HStack{
+                    Text(value.isEmpty ? type : value)
+                        .foregroundColor(value.isEmpty ? .gray : .black)
+                    Spacer()
+                    Image(systemName: "chevron.down")
+                        .foregroundColor(Color.orange)
+                        .font(Font.system(size: 20, weight: .bold))
+                }
+                .padding(.horizontal)
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(height: 2)
+            }
         }
     }
 }
