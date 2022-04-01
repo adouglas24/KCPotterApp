acff0a0f5dfce3fba7ff3e28ece91b168462ab31
daniel442li <daniel442li@gmail.com>
2022-04-01 17:31:25 -0500

    so many images

--- KC Potter/Backend/Database.swift
+++ KC Potter/Backend/Database.swift
@@ -49,7 +49,7 @@ func runExample() {
         case .failure(let error):
             print("Login failed: \(error.localizedDescription)")
         case .success(let user):
-            print("Successfully loggedasd in as user \(user)")
+            print("Successfully logged in as user \(user)")
             // Now logged in, do something with user
             // Remember to dispatch to main if you are doing anything on the UI thread
             onLogin(app)
