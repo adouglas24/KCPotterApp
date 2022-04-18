9db6e14e9ebd8c7fccd407988e23c4d58ab2ce87
zliang <zliang@petsmart.com>
2022-04-17 21:57:47 -0500

    style and link

--- KC Potter/Frontend/questionsPage.swift
+++ KC Potter/Frontend/questionsPage.swift
@@ -19,8 +19,8 @@ struct questionsPage : View {
 //    var resource = "I am looking for ..."
     var resourceList = ["I am looking for ...", "Athletics", "Counseling", "Crisis", "Educational", "Medical/Health", "Religious/Spiritual"]
     
-    @State var input = ["I am a ...", "I identify as ...", "I am looking for ..."]
-    
+//    @State var input = ["I am a ...", "I identify as ...", "I am looking for ..."]
+    @State var input = ["","",""]
     
     
 //    enum Affiliation: String, CaseIterable, Identifiable {
@@ -89,38 +89,54 @@ struct questionsPage : View {
                     }
                 //}.frame(alignment: .bottom)
             }
-            
-            
+//
+//            let queryFilter: Document = ["name": "Americano"]
+//            collection.find(filter: queryFilter) { result in
+//                switch result {
+//                case .failure(let error):
+//                    print("Call to MongoDB failed: \(error.localizedDescription)")
+//                    return
+//                case .success(let documents):
+//                    print("Results: ")
+//                    for document in documents {
+//                        print("Coffee drink: \(document)")
+//                    }
+//                }
+//            }
     
             
             // display resources
             VStack {
-                if input[0] == "I am a ..." && input[1] == "I identify as ..." && input[2] == "I am looking for ..." {
-                    Text("Please make selections")
-                } else if input[0] == "Community" || input[2] == "Counseling" {
-                    Text("[Melissa Gordon](https://www.newcounselingpllc.com/who-we-are-new) at Nashville Emotional Wellness")
-                    Text("[Rebecca Lesesne](https://www.rebecca-lesesne.com/appointments) – Private Practice")
-                    Text("[Matt Mills](http://mattmillscounseling.com/) – Private Practice")
-                    Text("[Carlton Cornett](https://carlton-cornett-lcsw.com/index.html) – Private Practice")
-                    Text("[Sasha-Nicole Cory](https://workscounselingcenter.com/sasha-nicole-cory/) – The Works")
-                    Text("[Cat Tourangeau](https://www.alliedbehavior.com/) – Allied Behavioral Health")
-                    Text("[Frank Byndloss](https://www.drfrankbyndloss.com/) – Private Practice")
-                    Text("[Alyssa Birmingham](https://www.thebriggsinstitute.com/alyssab) – the Briggs inst.")
-                    Text("[Aaron Byrd](http://byrdseyeviewtherapy.com/) – Byrds Eye View")
-                    Text("[Ruth Cohen](http://www.ruthcohen.net/about-ruth-cohen-nashville-counselor/) – Private Practice")
+                if input[0] == "" && input[1] == "" && input[2] == "" {
+                    Text("") // don't display anything on initial launch
+                } else if input[0] == "I am a ..." && input[1] == "I identify as ..." && input[2] == "I am looking for ..." {
+                    Text("Please make selections") // intentionally submit empty input
+                    
                 } else if input[2] == "Crisis" {
-                    Text("Vanderbilt")
-                    Text("[UCC Urgent Care Counseling](https://www.vanderbilt.edu/ucc/services/urgent-care-counseling/) – 615.322.2571")
-                    Text("VU Medical Center Emergency Services – 615.322.0160")
-                    Text("VU Police Department – 911 (emergency) or 615.421.1911 (non-emergency)")
-                    Text("National")
-                    Text("The Trevor Project – 866.488.7386")
-                    Text("National Suicide Prevention LifeLine – 800.273.8255")
-                    Text("Crisis Text Line – Text START to 741-741")
-                    Text("Gay, Lesbian, Bisexual, and Transgender National Hoteline – 888.843.4564")
-                    Text("GLBT National Youth Talkline – 800.246.7743")
+                    Text("Vanderbilt").font(.title)
+                    Text("[UCC Urgent Care Counseling](https://www.vanderbilt.edu/ucc/services/urgent-care-counseling/) – [615.322.2571](tel://6153222571)").accentColor(.blue)
+                    Text("VU Medical Center Emergency Services – [615.322.0160](tel://6153220160)").accentColor(.blue)
+                    Text("VU Police Department – [911](tel://911) (emergency) or [615.421.1911](tel://6154211911) (non-emergency)").accentColor(.blue)
+                    Text("National").padding(.top).font(.title)
+                    Text("The Trevor Project – [866.488.7386](tel://8664887386)").accentColor(.blue)
+                    Text("National Suicide Prevention LifeLine – [800.273.8255](tel://8002738255)").accentColor(.blue)
+                    Text("Crisis Text Line – Text START to 741-741").accentColor(.blue)
+                    Text("Gay, Lesbian, Bisexual, and Transgender National Hoteline – [888.843.4564](tel://8888434564)").accentColor(.blue)
+                    Text("GLBT National Youth Talkline – [800.246.7743](tel://8002467743)").accentColor(.blue)
+                    
+                } else if input[0] == "Community" || input[2] == "Counseling" {
+                    Text("[Melissa Gordon](https://www.newcounselingpllc.com/who-we-are-new) at Nashville Emotional Wellness").accentColor(.blue)
+                    Text("[Rebecca Lesesne](https://www.rebecca-lesesne.com/appointments) – Private Practice").accentColor(.blue)
+                    Text("[Matt Mills](http://mattmillscounseling.com/) – Private Practice").accentColor(.blue)
+                    Text("[Carlton Cornett](https://carlton-cornett-lcsw.com/index.html) – Private Practice").accentColor(.blue)
+                    Text("[Sasha-Nicole Cory](https://workscounselingcenter.com/sasha-nicole-cory/) – The Works").accentColor(.blue)
+                    Text("[Cat Tourangeau](https://www.alliedbehavior.com/) – Allied Behavioral Health").accentColor(.blue)
+                    Text("[Frank Byndloss](https://www.drfrankbyndloss.com/) – Private Practice").accentColor(.blue)
+                    Text("[Alyssa Birmingham](https://www.thebriggsinstitute.com/alyssab) – the Briggs inst.").accentColor(.blue)
+                    Text("[Aaron Byrd](http://byrdseyeviewtherapy.com/) – Byrds Eye View").accentColor(.blue)
+                    Text("[Ruth Cohen](http://www.ruthcohen.net/about-ruth-cohen-nashville-counselor/) – Private Practice").accentColor(.blue)
                 } else {
-                    Text("No resources found")
+                    Text("No resources found.")
                 }
                 
                 
